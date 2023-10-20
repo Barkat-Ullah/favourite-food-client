bd9428c61f9072d78f7eef6b9770483ca8800b4a
Barkat ullah <barkatullah585464@gmail.com>
2023-10-20 09:02:13 +0600

    get the existing product by there name

--- src/Pages/FoodCard/FoodCard.jsx
+++ src/Pages/FoodCard/FoodCard.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const FoodCard = ({ food }) => {
   console.log(food);
-  const {id, supplier, image} = food;
+  const {id, supplier_brand, image} = food;
 
   return (
     <div className="mb-5 ">
@@ -15,7 +15,7 @@ const FoodCard = ({ food }) => {
           <div className="hero-overlay bg-opacity-80"></div>
           <div className="hero-content text-center text-neutral-content">
             <div className="max-w-md">
-              <h1 className="mb-5 text-5xl font-bold">{supplier}</h1>
+              <h1 className="mb-5 text-5xl font-bold">{supplier_brand}</h1>
             </div>
           </div>
         </div>
